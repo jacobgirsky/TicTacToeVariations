3ff3a26b64a998ab9e7c0fb7e43f27df1a2f2176
jacob <jgirsky@my.apsu.edu>
2019-01-30 08:07:17 -0600

    Added design for how to play layout.

--- app/src/main/java/com/jacobgirsky/tictactoevariations/MainActivity.java
+++ app/src/main/java/com/jacobgirsky/tictactoevariations/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jacobgirsky.tictactoevariations;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.how_to_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToHowTo();
+            }
+        });
+    }
+
+    private void goToHowTo() {
+        Intent intent = new Intent(getApplicationContext(), activity_how_to.class);
+        startActivity(intent);
     }
 }
