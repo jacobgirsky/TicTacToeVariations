eb32c8872ee01d35d24f0d68ddd74d75109ef88e
jacob <jgirsky@my.apsu.edu>
2019-02-03 14:53:58 -0600

    Added layout for game.

--- app/src/main/java/com/jacobgirsky/tictactoevariations/NotaktoActivity.java
+++ app/src/main/java/com/jacobgirsky/tictactoevariations/NotaktoActivity.java
@@ -9,5 +9,7 @@ public class NotaktoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notakto);
+
+
     }
 }
