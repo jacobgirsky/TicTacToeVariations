b8794857a211eb03305bb11fda062062308c55bf
Bushrasi <bushra-si@hotmail.com>
2019-02-11 15:39:11 -0600

    new Notakto layout added!

--- app/src/main/java/com/jacobgirsky/tictactoevariations/NotaktoActivity.java
+++ app/src/main/java/com/jacobgirsky/tictactoevariations/NotaktoActivity.java
@@ -8,6 +8,6 @@ public class NotaktoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notakto);
+        setContentView(R.layout.activity2_notakto);
     }
 }
